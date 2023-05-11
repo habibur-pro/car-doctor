9b64f39bf21a6adf2db45defcc80a60556f9ab9c
habibur <va.habibur@gmail.com>
2023-05-11 08:59:57 +0600

    added about us

---
+++ src/pages/Home/About/About.jsx
@@ -0,0 +1,27 @@
+import person from '../../../assets/images/about_us/person.jpg'
+import parts from '../../../assets/images/about_us/parts.jpg'
+
+const About = () => {
+    return (
+        <div>
+            <div className="hero min-h-screen bg-base-200">
+                <div className="hero-content flex-col lg:flex-row gap-10">
+                    <div className='relative h-[550px] w-1/2 '>
+                        <img src={person} className="w-4/5 h-4/5 rounded-lg shadow-2xl" />
+                        <img src={parts} className="w-1/2 border-8 border-white absolute left-1/2 top-1/2 rounded-lg shadow-2xl" />
+                    </div>
+
+                    <div className='w-1/3  '>
+                        <h3 className='text-xl text-primary font-bold'>About Us</h3>
+                        <h1 className="text-5xl font-bold">We are qualified & of experience in this field</h1>
+                        <p className="py-6">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. </p>
+                        <p className="py-6">the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. </p>
+                        <button className="btn btn-primary">Get More Info</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default About;
--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -8,16 +8,51 @@ import { FaArrowRight, FaArrowLeft } from "react-icons/fa";
 const Banner = () => {
     return (
         <div>
-            <div className="carousel w-full max-h-[600px]">
+            <div className="carousel w-full h-[600px]">
                 <div id="slide1" className="carousel-item relative w-full">
                     <img src={image1} className="w-full" />
+
+
+                    {/* content  */}
+                    <div className="absolute flex items-center bg-gradient-to-tr from-[#151515] to-[rgba(21, 21, 21, 0)] w-full h-full justify-start  space-x-5    text-white space-y-7">
+                        <div className='pl-5 md:pl-20 space-y-6 md:w-1/2'>
+                            <h1 className='text-5xl md:text-6xl font-bold'>Affordable Price For Car Servicing</h1>
+                            <p>There are many variations of passages of  available, but the majority have suffered alteration in some form</p>
+
+                            <div className='space-x-10'>
+                                <button className='btn btn-outline btn-primary'>Discover more</button>
+                                <button className='btn btn-outline btn-primary'>Latest Projects</button>
+                            </div>
+                        </div>
+
+                    </div>
+
+                    {/* buttons  */}
                     <div className="absolute flex justify-end space-x-5 transform -translate-y-1/2 left-5 right-5 bottom-0">
                         <a href="#slide6" className="btn btn-primary btn-circle"><FaArrowLeft /></a>
                         <a href="#slide2" className="btn btn-primary btn-circle"><FaArrowRight /></a>
                     </div>
                 </div>
+
+
+                {/* sliede 2  */}
                 <div id="slide2" className="carousel-item relative w-full">
                     <img src={image2} className="w-full" />
+                    {/* content  */}
+                    <div className="absolute flex items-center bg-gradient-to-tr from-[#151515] to-[rgba(21, 21, 21, 0)] w-full h-full justify-start  space-x-5    text-white space-y-7">
+                        <div className='pl-5 md:pl-20 space-y-6 md:w-1/2'>
+                            <h1 className='text-5xl md:text-6xl font-bold'>Affordable Price For Car Servicing</h1>
+                            <p>There are many variations of passages of  available, but the majority have suffered alteration in some form</p>
+
+                            <div className='space-x-10'>
+                                <button className='btn btn-outline btn-primary'>Discover more</button>
+                                <button className='btn btn-outline btn-primary'>Latest Projects</button>
+                            </div>
+                        </div>
+
+                    </div>
+
+                    {/* buttons  */}
                     <div className="absolute flex justify-end space-x-5 transform -translate-y-1/2 left-5 right-5 bottom-0">
                         <a href="#slide1" className="btn btn-primary btn-circle"><FaArrowLeft /></a>
                         <a href="#slide3" className="btn btn-primary btn-circle"><FaArrowRight /></a>
@@ -25,6 +60,22 @@ const Banner = () => {
                 </div>
                 <div id="slide3" className="carousel-item relative w-full">
                     <img src={image3} className="w-full" />
+
+                    {/* content  */}
+                    <div className="absolute flex items-center bg-gradient-to-tr from-[#151515] to-[rgba(21, 21, 21, 0)] w-full h-full justify-start  space-x-5    text-white space-y-7">
+                        <div className='pl-5 md:pl-20 space-y-6 md:w-1/2'>
+                            <h1 className='text-5xl md:text-6xl font-bold'>Affordable Price For Car Servicing</h1>
+                            <p>There are many variations of passages of  available, but the majority have suffered alteration in some form</p>
+
+                            <div className='space-x-10'>
+                                <button className='btn btn-outline btn-primary'>Discover more</button>
+                                <button className='btn btn-outline btn-primary'>Latest Projects</button>
+                            </div>
+                        </div>
+
+                    </div>
+
+                    {/* buttons  */}
                     <div className="absolute flex justify-end space-x-5 transform -translate-y-1/2 left-5 right-5 bottom-0">
                         <a href="#slide2" className="btn btn-primary btn-circle"><FaArrowLeft /></a>
                         <a href="#slide4" className="btn btn-primary btn-circle"><FaArrowRight /></a>
@@ -32,6 +83,21 @@ const Banner = () => {
                 </div>
                 <div id="slide4" className="carousel-item relative w-full">
                     <img src={image4} className="w-full" />
+                    {/* content  */}
+                    <div className="absolute flex items-center bg-gradient-to-tr from-[#151515] to-[rgba(21, 21, 21, 0)] w-full h-full justify-start  space-x-5    text-white space-y-7">
+                        <div className='pl-5 md:pl-20 space-y-6 md:w-1/2'>
+                            <h1 className='text-5xl md:text-6xl font-bold'>Affordable Price For Car Servicing</h1>
+                            <p>There are many variations of passages of  available, but the majority have suffered alteration in some form</p>
+
+                            <div className='space-x-10'>
+                                <button className='btn btn-outline btn-primary'>Discover more</button>
+                                <button className='btn btn-outline btn-primary'>Latest Projects</button>
+                            </div>
+                        </div>
+
+                    </div>
+
+                    {/* buttons  */}
                     <div className="absolute flex justify-end space-x-5 transform -translate-y-1/2 left-5 right-5 bottom-0">
                         <a href="#slide3" className="btn btn-primary btn-circle"><FaArrowLeft /></a>
                         <a href="#slide5" className="btn btn-primary btn-circle"><FaArrowRight /></a>
@@ -39,6 +105,21 @@ const Banner = () => {
                 </div>
                 <div id="slide5" className="carousel-item relative w-full">
                     <img src={image5} className="w-full" />
+                    {/* content  */}
+                    <div className="absolute flex items-center bg-gradient-to-tr from-[#151515] to-[rgba(21, 21, 21, 0)] w-full h-full justify-start  space-x-5    text-white space-y-7">
+                        <div className='pl-5 md:pl-20 space-y-6 md:w-1/2'>
+                            <h1 className='text-5xl md:text-6xl font-bold'>Affordable Price For Car Servicing</h1>
+                            <p>There are many variations of passages of  available, but the majority have suffered alteration in some form</p>
+
+                            <div className='space-x-10'>
+                                <button className='btn btn-outline btn-primary'>Discover more</button>
+                                <button className='btn btn-outline btn-primary'>Latest Projects</button>
+                            </div>
+                        </div>
+
+                    </div>
+
+                    {/* buttons  */}
                     <div className="absolute flex justify-end space-x-5 transform -translate-y-1/2 left-5 right-5 bottom-0">
                         <a href="#slide4" className="btn btn-primary btn-circle"><FaArrowLeft /></a>
                         <a href="#slide6" className="btn btn-primary btn-circle"><FaArrowRight /></a>
@@ -46,6 +127,21 @@ const Banner = () => {
                 </div>
                 <div id="slide6" className="carousel-item relative w-full">
                     <img src={image6} className="w-full" />
+                    {/* content  */}
+                    <div className="absolute flex items-center bg-gradient-to-tr from-[#151515] to-[rgba(21, 21, 21, 0)] w-full h-full justify-start  space-x-5    text-white space-y-7">
+                        <div className='pl-5 md:pl-20 space-y-6 md:w-1/2'>
+                            <h1 className='text-5xl md:text-6xl font-bold'>Affordable Price For Car Servicing</h1>
+                            <p>There are many variations of passages of  available, but the majority have suffered alteration in some form</p>
+
+                            <div className='space-x-10'>
+                                <button className='btn btn-outline btn-primary'>Discover more</button>
+                                <button className='btn btn-outline btn-primary'>Latest Projects</button>
+                            </div>
+                        </div>
+
+                    </div>
+
+                    {/* buttons  */}
                     <div className="absolute flex justify-end space-x-5 transform -translate-y-1/2 left-5 right-5 bottom-0">
                         <a href="#slide5" className="btn btn-primary btn-circle"><FaArrowLeft /></a>
                         <a href="#slide1" className="btn btn-primary btn-circle"><FaArrowRight /></a>
